b4f195ac95984b65ca43de047fa0ff0730e54ce9
Andrea Censi <acensi@ethz.ch>
2020-12-22 19:51:33 +0100

    fix

--- Makefile
+++ Makefile
@@ -11,8 +11,9 @@ upload:
 	rm -f dist/*
 	rm -rf src/*.egg-info
 	python3 setup.py sdist
-	twine upload dist/*
-
+	devpi use $(TWINE_REPOSITORY_URL)
+	devpi login $(TWINE_USERNAME) --password $(TWINE_PASSWORD)
+	devpi upload --verbose dist/*
 bump-upload:
 	$(MAKE) bump
 	$(MAKE) upload
@@ -29,4 +30,3 @@ test-python3-install:
 	pip install -r requirements.txt
 	pip install nose
 	python setup.py develop --no-deps
-
--- setup.py
+++ setup.py
@@ -5,20 +5,21 @@ from setuptools import find_packages, setup
 
 def get_version(filename):
     import ast
+
     version = None
     with open(filename) as f:
         for line in f:
-            if line.startswith('__version__'):
+            if line.startswith("__version__"):
                 version = ast.parse(line).body[0].value.s
                 break
         else:
-            raise ValueError('No version found in %r.' % filename)
+            raise ValueError("No version found in %r." % filename)
     if version is None:
         raise ValueError(filename)
     return version
 
 
-version = get_version('src/quickapp/__init__.py')
+version = get_version("src/quickapp/__init__.py")
 
 description = """"""
 
@@ -27,47 +28,44 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-long_description = read('README.rst')
+long_description = read("README.rst")
 
-line = 'z6'
+line = "z7"
 
 install_requires = [
-    'compmake-z6',
-    'reprep-z6',
-    'PyContracts3',
-    'DecentLogs-z6',
-    'ConfTools-z6',
+    "compmake-z7",
+    "reprep-z7",
+    "PyContracts3",
+    "DecentLogs-z7",
+    "ConfTools-z7",
 ]
 
-setup(name=f'QuickApp-{line}',
-      author="Andrea Censi",
-      author_email="",
-      url='http://github.com/AndreaCensi/quickapp',
-
-      description=description,
-      long_description=long_description,
-      keywords="",
-      license="",
-
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          # 'Intended Audience :: Developers',
-          # 'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-          # 'Topic :: Software Development :: Quality Assurance',
-          # 'Topic :: Software Development :: Documentation',
-          # 'Topic :: Software Development :: Testing'
-      ],
-
-      version=version,
-      download_url='http://github.com/AndreaCensi/quickapp/tarball/%s' % version,
-
-      entry_points={
-          'console_scripts': [
-              # 'comptests = comptests:main_comptests'
-          ]
-      },
-      package_dir={'': 'src'},
-      packages=find_packages('src'),
-      install_requires=install_requires,
-      tests_require=['nose'],
-      )
+setup(
+    name=f"QuickApp-{line}",
+    author="Andrea Censi",
+    author_email="",
+    url="http://github.com/AndreaCensi/quickapp",
+    description=description,
+    long_description=long_description,
+    keywords="",
+    license="",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        # 'Intended Audience :: Developers',
+        # 'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        # 'Topic :: Software Development :: Quality Assurance',
+        # 'Topic :: Software Development :: Documentation',
+        # 'Topic :: Software Development :: Testing'
+    ],
+    version=version,
+    download_url="http://github.com/AndreaCensi/quickapp/tarball/%s" % version,
+    entry_points={
+        "console_scripts": [
+            # 'comptests = comptests:main_comptests'
+        ]
+    },
+    package_dir={"": "src"},
+    packages=find_packages("src"),
+    install_requires=install_requires,
+    tests_require=["nose"],
+)
