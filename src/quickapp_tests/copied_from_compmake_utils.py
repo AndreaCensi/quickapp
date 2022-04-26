a885c53e16383755681a7144c2f0caba526be45a
Andrea Censi <acensi@ethz.ch>
2022-04-26 12:39:16 +0200

    [ci skip] advances

--- src/quickapp_tests/copied_from_compmake_utils.py
+++ src/quickapp_tests/copied_from_compmake_utils.py
@@ -22,8 +22,7 @@ from compmake import (
 from zuper_commons.cmds import ExitCode
 from zuper_commons.types import ZAssertionError, ZException, ZValueError
 from zuper_utils_asyncio import create_sync_task2, SyncTaskInterface
-from zuper_zapp import async_run_timeout, setup_environment2
-from zuper_zapp.utils import with_log_control
+from zuper_zapp import async_run_timeout, setup_environment2, with_log_control
 
 X = TypeVar("X")
 
