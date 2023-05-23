2d40416bf00e6d31421e15a918b12c3f03462850
Andrea Censi <AndreaCensi@users.noreply.github.com>
2023-05-23 21:52:58 +0200

    ciskip

--- src/quickapp_tests/copied_from_compmake_utils.py
+++ src/quickapp_tests/copied_from_compmake_utils.py
@@ -2,9 +2,18 @@ import os
 import traceback
 from contextlib import asynccontextmanager
 from tempfile import mkdtemp
-from typing import AsyncIterator, Awaitable, Callable, cast, Collection, List, Optional, TypeVar
-
-from typing_extensions import Concatenate, ParamSpec
+from typing import (
+    AsyncIterator,
+    Awaitable,
+    Callable,
+    cast,
+    Collection,
+    Concatenate,
+    List,
+    Optional,
+    ParamSpec,
+    TypeVar,
+)
 
 from compmake import (
     all_jobs,
@@ -85,7 +94,6 @@ class Env:
         my_assert_equal(first, second, msg)
 
     async def assert_jobs_equal(self, expr: str, jobs, ignore_dyn_reports: bool = True):
-
         # js = 'not-valid-yet'
         js = await self.get_jobs(expr)
         if ignore_dyn_reports:
@@ -191,13 +199,11 @@ def run_with_env(f: Callable[[Env], Awaitable[ExitCode]]) -> Callable[[], ExitCo
             # sti.set_fs(LocalFS(cwd, allow_up=True, sti=sti))
 
             async with setup_environment2(sti, working_dir=cwd):
-
                 async with with_log_control(False):  # XXX
                     async with environment(sti) as env:
                         try:
                             res = await f(env)
                         except BaseException:
-
                             sti.logger.error(traceback.format_exc())
                             sti.logger.error("these are some stats", all_jobs=await env.all_jobs())
 
