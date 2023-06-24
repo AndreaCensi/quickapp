e38550037e2d1d3f5766623d587370d60f780b5f
Andrea Censi <AndreaCensi@users.noreply.github.com>
2023-06-24 13:59:37 +0200

    ciskip

--- src/quickapp_tests/test_dynamic_1.py
+++ src/quickapp_tests/test_dynamic_1.py
@@ -1,4 +1,4 @@
-from nose.tools import assert_equal
+from zuper_commons.test_utils import my_assert_equal
 
 from compmake import Context
 from quickapp import QuickApp, QuickAppContext, DecentParams
